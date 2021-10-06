3480388b84450745b2febeb29b71d54919f550ef
Andrews-Hef <andrews.agricole@gmail.com>
2021-10-06 16:08:23 +0200

    fin video 5

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
@@ -24,20 +29,61 @@ class AdController extends AbstractController
             'ads'=>$ads
         ]);
     }
-    /**
+   
+
+
+
+     /**
+      * permet de créer une annonce 
+      * 
+      * @Route("/ads/new" , name="ads_create")
+      * @return Response
+      */
+    public function create(Request $request, EntityManagerInterface $manager){
+        $ad = new Ad();
+
+
+        $form= $this->createForm(AdType::class,$ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid() ){
+            //$manager =$this->getDoctrine()->getManager();
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "l'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+            
+
+            return $this->redirectToRoute('ads_show',[
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+
+        return $this->render('ad/new.html.twig',[
+            'form'=> $form->createView()
+        ]);
+  }
+
+   /**
      * permet d'afficher une seule annonce
      *
      * @Route("/ads/{slug}", name="ads_show")
      * 
      * @return Response
      */
-    public function show($slug,AdRepository $repo){
+    public function show(Ad $ad){
        //je recupere l'annonce du slug
-        $ad =$repo->findOneBySlug($slug);
+       // $ad =$repo->findOneBySlug($slug);
 
         return $this->render('ad/show.html.twig',[
             'ad'=>$ad
         ]);
     }
+     
 }
-//video4  27 min
---
+++ src/Form/AdType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Doctrine\DBAL\Types\TextType as DoctrineTextType;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+class AdType extends AbstractType
+{
+
+    /**
+     * permet d'avoir la configurtion de base dun champ
+     * 
+     *
+     * @param [type] $label
+     * @param [type] $placeholder
+     * @return array
+     */
+private function getConfiguration($label,$placeholder){
+  return[
+        'label'=> $label,
+        'attr' => [
+            'placeholder' => $placeholder
+        ]
+    ];
+
+}
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, $this->getConfiguration("Titre","Tapez un super titre !"))
+            ->add('slug', TextType::class,$this->getConfiguration("Adresse web","Tapez l'adressse web (automatique)"))
+            ->add('price', MoneyType::class,$this->getConfiguration("Prix par nuit","indiquez le prix que vous voulez pour une nuit") )
+            ->add('introduction',TextType::class,$this->getConfiguration("introduction","donnez une description globale de l'annonce"))
+            ->add('content',TextareaType::class, $this->getConfiguration("description detaillé","tapez une description qui donne vriament envie de venir chez vous!"))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image","donnez une jolie image") )
+            ->add('rooms', IntegerType::class,$this->getConfiguration("nombre de chambrs","le nombre de chambes disponible ") )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
