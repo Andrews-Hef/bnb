c9bdde63194696ab40c85fae6eb627a18e91e22d
Andrews-Hef <andrews.agricole@gmail.com>
2021-11-09 12:07:01 +0100

    ziak

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -49,7 +49,8 @@ private function getConfiguration($label,$placeholder){
             ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image","donnez une jolie image") )
             ->add('rooms', IntegerType::class,$this->getConfiguration("nombre de chambrs","le nombre de chambes disponible ") )
             ->add('images',CollectionType::class,[
-              'entry_type'=>  ImageType::class
+              'entry_type'=>  ImageType::class,
+              'allow_add'=> true 
               
             ])
         ;
